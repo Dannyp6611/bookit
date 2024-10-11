cbd86d0b195fecaef9036d30196f39e0bade76a0
Dannyp6611 <danielparker96@hotmail.co.uk>
2024-10-11 12:45:45 +0100

    added json data and dynamic room page routes

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
+import rooms from "@/data/rooms.json";
+import RoomCard from "@/components/RoomCard";
+import Heading from "@/components/Heading";
+
 const HomePage = () => {
   return (
     <>
-      <h1>Bookit</h1>
+      <Heading title="Available Rooms" />
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard key={room.$id} room={room} />)
+      ) : (
+        <p>No rooms available at the moment</p>
+      )}
     </>
   );
 };
